24f309979a963b89d9f5bd9cf51d0be8c6e13691
Cuong Le <cuongle@bitmark.com>
2017-05-18 15:02:14 +0700

    Move tests to corresponding projects

--- browser/test.js
+++ browser/test.js
@@ -1,26 +1,219 @@
-let nodeServer = require('../server-node');
+let Helper = {};
+Helper.createSocketConnection = (port, onOpenCallback) => {
+  let wrpc = new SimpleWRPC('ws:\/\/127.0.0.1:' + port + '\/');
+  wrpc.on('open', onOpenCallback);
+  return wrpc;
+}
 
-var BitmarkRPCWebsocketServer = nodeServer.Server;
-var bitmarkRPC = new BitmarkRPCWebsocketServer({port: 3000});
+Helper.portMapping = {
+  'TEST-01': 8201,
+  'TEST-02': 8202,
+  'TEST-03': 8203,
+  'TEST-04': 8204,
+  'TEST-05': 8205,
+  'TEST-06': 8206,
+  'TEST-07': 8207,
+  'TEST-08': 8208,
+  'TEST-09': 8209,
+  'TEST-10': 8210
+}
 
-bitmarkRPC.on('connection', function(conn) {
-  conn.subscribeForEvent('mydata', function(data) {
-    console.log('I JUST RECEIVED MYDATA WITH DATA', data);
+console.log('Please start the server to test the fetures');
+
+describe('Basic pub/sub and method call', function() {
+  this.timeout(30000);
+
+  beforeEach((done) => {
+    // delay each test for the server to initialize first
+    setTimeout(done, 3000);
   });
-  conn.addListenerToMethodCall('mymethod', function(event) {
-    event.done({data: 'aaabc'});
+
+  let testName = 'TEST-01';
+  it(testName, function(done) {
+    let wrpc = Helper.createSocketConnection(Helper.portMapping[testName], () => {
+      wrpc.close();
+      done();
+    })
   });
-  conn.publishEvent('datafromserver', {a: 'aaa', b: 'bbb'}, function() {
-    console.log('Sent');
+
+  testName = 'TEST-02';
+  it(testName, function(done) {
+    let wrpc = Helper.createSocketConnection(Helper.portMapping[testName], () => {
+      wrpc.on('close', () => {
+        done();
+      });
+    })
   });
+
+  testName = 'TEST-03';
+  it(testName, function(done) {
+    let wrpc = Helper.createSocketConnection(Helper.portMapping[testName], () => {
+      wrpc.subscribeForEvent('test3-server-event', (event) => {
+        expect(event.data).to.deep.equal({greeting: "hi! I am websocket server"});
+        wrpc.close();
+        done();
+      });
+    })
+  });
+
+  testName = 'TEST-04';
+  it(testName, function(done) {
+    let wrpc = Helper.createSocketConnection(Helper.portMapping[testName], () => {
+      wrpc.addListenerToMethodCall('test4-server-call', (event) => {
+        expect(event.data).to.deep.equal({ask: "client, how are you?"});
+        event.done({answer: "I am fine."});
+        wrpc.close();
+        done();
+      });
+    })
+  });
+
+
+  testName = 'TEST-05';
+  it(testName, function(done) {
+    let wrpc = Helper.createSocketConnection(Helper.portMapping[testName], () => {
+      wrpc.publishEvent(`test5-client-event`, {greeting: "hi! I am websocket client"}, () => {
+        wrpc.close();
+        done();
+      });
+    });
+  });
+
+  testName = 'TEST-06';
+  it(testName, function(done) {
+    let wrpc = Helper.createSocketConnection(Helper.portMapping[testName], () => {
+      wrpc.callMethod(`test6-client-call`, {ask: "server, how are you?"}, (error, data) => {
+        expect(error).to.not.be.ok;
+        expect(data).to.deep.equal({answer: "I am fine."});
+        wrpc.close();
+        done();
+      });
+    });
+  });
+
 });
 
-// Run browser test by karma
-const cfg = require('karma').config;
-const karmaConfig = cfg.parseConfig(path.resolve('./karma.conf.js'));
-var KarmaServer = require('karma').Server
-var server = new KarmaServer(karmaConfig, function(exitCode) {
-  // console.log('Karma has exited with ' + exitCode)
-  // process.exit(exitCode)
+
+describe('Massive messages test', function() {
+  this.timeout(900000);
+
+  beforeEach((done) => {
+    // delay each test for the server to initialize first
+    setTimeout(done, 3000);
+  });
+
+  let massiveMessagesTestHelper = {
+    getRandomInt: (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    addEventHandler: (index, wrpc, callback) => {
+      wrpc.subscribeForEvent(`test9-server-event-${index}`, (event) => {
+        expect(event.data).to.deep.equal({greeting: `hello from server for server-event-${index}`});
+        callback();
+      });
+    },
+    publishEvent: (index, wrpc, callback) => {
+      setTimeout(() => {
+        wrpc.publishEvent(`test9-client-event-${index}`, {greeting: `hello from client for client-event-${index}`}, callback);
+      }, massiveMessagesTestHelper.getRandomInt(0,60000));
+    },
+    callMethod: (index, wrpc, callback) => {
+      setTimeout(() => {
+        wrpc.callMethod(`test9-client-call-${index}`, {ask: `From client-call-${index}, how are you?`}, (error, data) => {
+          console.log('CALL BACK!!!');
+          expect(error).to.not.be.ok;
+          expect(data).to.deep.equal({answer: `For client-call-${index}, I am so so.`});
+          callback();
+        });
+      }, massiveMessagesTestHelper.getRandomInt(0,60000));
+    },
+    addMethodCallHandler: (index, wrpc, callback) => {
+      wrpc.addListenerToMethodCall(`test9-server-call-${index}`, (event) => {
+        expect(event.data).to.deep.equal({ask: `From server-call-${index}, how are you?`});
+        event.done({answer: `For server-call-${index}, I am so so.`});
+        callback();
+      });
+    },
+    testFulFill: (wrpc, loopTarget, totalEventPublished, totalEventReceived, totalMethodCalled, totalMethodCallReceived, callback) => {
+      let enoughEventPublished = (totalEventPublished === loopTarget);
+      let enoughEventReceived = (totalEventReceived === loopTarget);
+      let enoughMethodCalled = (totalMethodCalled === loopTarget);
+      let enoughMethodCallReceived = (totalMethodCallReceived === loopTarget);
+      if (enoughEventPublished && enoughEventReceived && enoughMethodCalled && enoughMethodCallReceived) {
+        callback();
+      }
+    }
+  }
+
+  let testName = 'TEST-07';
+  it(testName, function(done) {
+    let loop = 100;
+    let totalEventPublished = 0;
+    let totalEventReceived = 0;
+    let totalMethodCalled = 0;
+    let totalMethodCallReceived = 0;
+
+    let wrpc = Helper.createSocketConnection(Helper.portMapping[testName], () => {
+      let checkTestFinish = () => {
+        massiveMessagesTestHelper.testFulFill(wrpc, loop, totalEventPublished, totalEventReceived, totalMethodCalled, totalMethodCallReceived, () => {
+          wrpc.close();
+          done();
+        })
+      }
+      for (let i = 1; i <= loop; i++) {
+        massiveMessagesTestHelper.publishEvent(i, wrpc, () => {
+          totalEventPublished++;
+          checkTestFinish();
+        });
+        massiveMessagesTestHelper.addEventHandler(i, wrpc, () => {
+          totalEventReceived++;
+          checkTestFinish();
+        });
+        massiveMessagesTestHelper.callMethod(i, wrpc, () => {
+          totalMethodCalled++;
+          checkTestFinish();
+        });
+        massiveMessagesTestHelper.addMethodCallHandler(i, wrpc, () => {
+          totalMethodCallReceived++;
+          checkTestFinish();
+        });
+      }
+    });
+  });
+
+  // testName = 'TEST-10';
+  // it(testName, function(done) {
+  //   let loop = 5;
+  //   let totalEventPublished = 0;
+  //   let totalEventReceived = 0;
+  //   let totalMethodCalled = 0;
+  //   let totalMethodCallReceived = 0;
+
+  //   let wrpc = Helper.createSocketConnection(Helper.portMapping[testName], () => {
+  //     let checkTestFinish = () => {
+  //       massiveMessagesTestHelper.testFulFill(wrpc, loop, totalEventPublished, totalEventReceived, totalMethodCalled, totalMethodCallReceived, () => {
+  //         wrpc.close();
+  //         done();
+  //       })
+  //     }
+  //     for (let i = 1; i <= loop; i++) {
+  //       massiveMessagesTestHelper.publishEvent(i, wrpc, () => {
+  //         totalEventPublished++;
+  //         checkTestFinish();
+  //       });
+  //       massiveMessagesTestHelper.addEventHandler(i, wrpc, () => {
+  //         totalEventReceived++;
+  //         checkTestFinish();
+  //       });
+  //       massiveMessagesTestHelper.callMethod(i, wrpc, () => {
+  //         totalMethodCalled++;
+  //         checkTestFinish();
+  //       });
+  //       massiveMessagesTestHelper.addMethodCallHandler(i, wrpc, () => {
+  //         totalMethodCallReceived++;
+  //         checkTestFinish();
+  //       });
+  //     }
+  //   });
+  // });
 });
-server.start();
--- server-node/test.js
+++ server-node/test.js
@@ -1,18 +1,225 @@
-var BitmarkRPCWebsocketServer = require('./index.js').Server;
+let chai = require('chai');
+let expect = chai.expect;
 
-var bitmarkRPC = new BitmarkRPCWebsocketServer({port: 3000});
+let nodeServer = require('../server-node');
+let SimpleWRPCServer = nodeServer.Server;
 
-bitmarkRPC.on('connection', function(conn) {
-  conn.subscribeForEvent('mydata', function(data) {
-    console.log('I JUST RECEIVED MYDATA WITH DATA', data);
+let Helper = {};
+Helper.createWRPCServer = (port, onConnectionCallback) => {
+  let wrpcServer = new SimpleWRPCServer({port: port});
+  wrpcServer.on('connection', onConnectionCallback);
+  return wrpcServer;
+}
+Helper.portMapping = {
+  'TEST-01': 8201,
+  'TEST-02': 8202,
+  'TEST-03': 8203,
+  'TEST-04': 8204,
+  'TEST-05': 8205,
+  'TEST-06': 8206,
+  'TEST-07': 8207,
+  'TEST-08': 8208
+}
+
+console.log('Please start the client to test the fetures');
+
+describe('Basic pub/sub and method call', function() {
+  this.timeout(30000);
+
+  let testName = 'TEST-01';
+  it(testName, function(done) {
+    let wrpcServer = Helper.createWRPCServer(Helper.portMapping[testName], (swrpcConn) => {
+      swrpcConn.on('close', function() {
+        wrpcServer.close();
+        done();
+      });
+    });
   });
-  conn.addListenerToMethodCall('mymethod', function(event) {
-    event.done({data: 'aaabc'});
+
+  testName = 'TEST-02';
+  it(testName, function(done) {
+    let wrpcServer = Helper.createWRPCServer(Helper.portMapping[testName], (swrpcConn) => {
+      swrpcConn.close();
+      wrpcServer.close();
+      done();
+    });
   });
-  conn.publishEvent('datafromserver', {a: 'aaa', b: 'bbb'}, function() {
-    console.log('Sent');
+
+  testName = 'TEST-03';
+  it(testName, function(done) {
+    let wrpcServer = Helper.createWRPCServer(Helper.portMapping[testName], (swrpcConn) => {
+      let testFinished = false;
+      swrpcConn.publishEvent(`test3-server-event`, {greeting: "hi! I am websocket server"}, () => {
+        testFinished = true;
+      });
+      swrpcConn.on('close', () => {
+        expect(testFinished).to.equal(true);
+        wrpcServer.close();
+        done();
+      })
+    });
   });
-  setTimeout(function() {
-    conn.close();
-  }, 5000);
+
+  testName = 'TEST-04';
+  it(testName, function(done) {
+    let wrpcServer = Helper.createWRPCServer(Helper.portMapping[testName], (swrpcConn) => {
+      let testFinished = false;
+
+      swrpcConn.callMethod(`test4-server-call`, {ask: "client, how are you?"}, (error, data) => {
+        expect(error).to.not.be.ok;
+        expect(data).to.deep.equal({answer: "I am fine."});
+        testFinished = true;
+      });
+      swrpcConn.on('close', () => {
+        expect(testFinished).to.equal(true);
+        wrpcServer.close();
+        done();
+      })
+    });
+  });
+
+
+  testName = 'TEST-05';
+  it(testName, function(done) {
+    let wrpcServer = Helper.createWRPCServer(Helper.portMapping[testName], (swrpcConn) => {
+      let testFinished = false;
+
+      swrpcConn.subscribeForEvent(`test5-client-event`, (event) => {
+        expect(event.data).to.deep.equal({greeting: "hi! I am websocket client"});
+        testFinished = true;
+      });
+      swrpcConn.on('close', () => {
+        expect(testFinished).to.equal(true);
+        wrpcServer.close();
+        done();
+      })
+    });
+  });
+
+  testName = 'TEST-06';
+  it(testName, function(done) {
+    let wrpcServer = Helper.createWRPCServer(Helper.portMapping[testName], (swrpcConn) => {
+      let testFinished = false;
+
+      swrpcConn.addListenerToMethodCall(`test6-client-call`, (event) => {
+        expect(event.data).to.deep.equal({ask: "server, how are you?"});
+        event.done({answer: "I am fine."});
+        testFinished = true;
+      });
+      swrpcConn.on('close', () => {
+        expect(testFinished).to.equal(true);
+        wrpcServer.close();
+        done();
+      })
+    });
+  });
+});
+
+describe('Massive messages test', function() {
+  this.timeout(900000);
+
+  let massiveMessagesTestHelper = {
+    getRandomInt: (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    addEventHandler: (index, swrpcConn, callback) => {
+      swrpcConn.subscribeForEvent(`test9-client-event-${index}`, (event) => {
+        expect(event.data).to.deep.equal({greeting: `hello from client for client-event-${index}`});
+        callback();
+      });
+    },
+    publishEvent: (index, wrpcConn, callback) => {
+      setTimeout(() => {
+        wrpcConn.publishEvent(`test9-server-event-${index}`, {greeting: `hello from server for server-event-${index}`}, callback);
+      }, massiveMessagesTestHelper.getRandomInt(0,60000));
+    },
+    addMethodCallHandler: (index, wrpcConn, callback) => {
+      wrpcConn.addListenerToMethodCall(`test9-client-call-${index}`, (event) => {
+        expect(event.data).to.deep.equal({ask: `From client-call-${index}, how are you?`});
+        event.done({answer: `For client-call-${index}, I am so so.`});
+        callback();
+      });
+    },
+    callMethod: (index, wrpcConn, callback) => {
+      setTimeout(() => {
+        wrpcConn.callMethod(`test9-server-call-${index}`, {ask: `From server-call-${index}, how are you?`}, (error, data) => {
+          expect(error).to.not.be.ok;
+          expect(data).to.deep.equal({answer: `For server-call-${index}, I am so so.`});
+          callback();
+        });
+      }, massiveMessagesTestHelper.getRandomInt(0,60000));
+    }
+  };
+
+  let testName = 'TEST-07';
+  it(testName, (done) => {
+    let wrpcServer = Helper.createWRPCServer(Helper.portMapping[testName], (swrpcConn) => {
+      let loop = 100;
+      let totalEventReceived = 0;
+      let totalEventPublished = 0;
+      let totalMethodCallReceived = 0;
+      let totalMethodCalled = 0;
+
+      for (let i = 1; i <= loop; i++) {
+        massiveMessagesTestHelper.addEventHandler(i, swrpcConn, () => {
+          totalEventReceived++;
+        });
+        massiveMessagesTestHelper.publishEvent(i, swrpcConn, () => {
+          totalEventPublished++;
+        });
+        massiveMessagesTestHelper.addMethodCallHandler(i, swrpcConn, () => {
+          totalMethodCallReceived++;
+        });
+        massiveMessagesTestHelper.callMethod(i, swrpcConn, () => {
+          totalMethodCalled++;
+        });
+      }
+
+      swrpcConn.on('close', () => {
+        expect(totalEventReceived).to.equal(loop);
+        expect(totalEventPublished).to.equal(loop);
+        expect(totalMethodCallReceived).to.equal(loop);
+        expect(totalMethodCalled).to.equal(loop);
+        wrpcServer.close();
+        done();
+      });
+    });
+  });
+
+
+  // testName = 'TEST-08';
+  // it(testName, (done) => {
+  //   let wrpcServer = Helper.createWRPCServer(Helper.portMapping[testName], (swrpcConn) => {
+  //     let loop = 5;
+  //     let totalEventReceived = 0;
+  //     let totalEventPublished = 0;
+  //     let totalMethodCallReceived = 0;
+  //     let totalMethodCalled = 0;
+
+  //     for (let i = 1; i <= loop; i++) {
+  //       massiveMessagesTestHelper.addEventHandler(i, swrpcConn, () => {
+  //         totalEventReceived++;
+  //       });
+  //       massiveMessagesTestHelper.publishEvent(i, swrpcConn, () => {
+  //         totalEventPublished++;
+  //       });
+  //       massiveMessagesTestHelper.addMethodCallHandler(i, swrpcConn, () => {
+  //         totalMethodCallReceived++;
+  //       });
+  //       massiveMessagesTestHelper.callMethod(i, swrpcConn, () => {
+  //         totalMethodCalled++;
+  //       });
+  //     }
+
+  //     swrpcConn.on('close', () => {
+  //       expect(totalEventReceived).to.equal(loop);
+  //       expect(totalEventPublished).to.equal(loop);
+  //       expect(totalMethodCallReceived).to.equal(loop);
+  //       expect(totalMethodCalled).to.equal(loop);
+  //       wrpcServer.close();
+  //       done();
+  //     });
+  //   });
+  // });
+
 });
